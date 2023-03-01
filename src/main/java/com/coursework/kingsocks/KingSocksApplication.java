d8f9cab30aa1abb5dc23a20f55b776828ad41114
1 <sergey.trbv@yandex.ru>
2023-03-01 20:47:34 +0400

    Первый коммит

---
+++ src/main/java/com/coursework/kingsocks/KingSocksApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.kingsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingSocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingSocksApplication.class, args);
+    }
+
+}
